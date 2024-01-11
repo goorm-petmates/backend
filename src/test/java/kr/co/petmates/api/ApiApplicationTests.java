2c943e660b21a42724766053b4523b0d1d25f538
Timothy <timothy@jungnanum.com>
2024-01-12 03:41:23 +0900

    chore(all): 프로젝트 생성
    "https://start.spring.io"에서 프로젝트 생성

---
+++ src/test/java/kr/co/petmates/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.petmates.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
