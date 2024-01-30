4a093b42e121133a9c1c5b298b3f36d1abc08e8b
hyorim-luna-choi <hyolimjj@naver.com>
2024-01-30 22:56:02 +0900

    feat(#4): 펫 정보 등록

---
+++ src/main/java/kr/co/petmates/api/bussiness/pet/dto/PetDto.java
@@ -0,0 +1,61 @@
+package kr.co.petmates.api.bussiness.pet.dto;
+
+import com.nimbusds.openid.connect.sdk.claims.Gender;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import kr.co.petmates.api.bussiness.pet.entity.Pet;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PetDto {
+
+    private Long id; // 시퀀스
+
+    @NotNull
+    @Pattern(regexp = "^[가-힣_]{1,10}$")
+    private String name; // 이름
+
+    @NotNull
+    @Pattern(regexp = "^[가-힣_]{1,10}$")
+    private String breed; // 견종
+
+    @NotNull
+    @Pattern(regexp = "^(19[0-9]{2}|20[0-2][0-9]|202[0-4])$", message = "출생년도는 1900년부터 2024년 사이여야 합니다.")
+    private String birthYear; // 출생년도
+
+    @NotNull
+    @Pattern(regexp = "^[0-9]{1,2}$|^(100)$\n")
+    private String weight; // 몸무게
+
+    private Gender sex; // 성별
+
+    private boolean isNeutering; // 중성화
+
+    private boolean isAllergy; // 알러지
+
+    private boolean isDisease; // 질병
+
+    private String ect; // 참고사항
+
+    public static PetDto toPetDto(Pet pet) { // entity -> dto
+        PetDto petDto = new PetDto();
+        petDto.setName(pet.getName());
+        petDto.setBreed(pet.getBreed());
+        petDto.setBirthYear(pet.getBirthYear());
+        petDto.setWeight(pet.getWeight());
+        petDto.setSex(pet.getSex());
+        petDto.setEct(pet.getEct());
+
+        return petDto;
+    }
+
+    // 파일 URL 필드
+    private String photoUrl;
+}
+
