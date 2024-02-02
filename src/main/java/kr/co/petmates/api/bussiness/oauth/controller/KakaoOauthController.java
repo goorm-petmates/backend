3e32a749341fa482b081a32b7f96b1659e62c1c7
jhrogh <wogml5031@gmail.com>
2024-02-02 21:06:00 +0900

    feat(#12): KakaoOauthController 인가코드 받아오기 수정

--- src/main/java/kr/co/petmates/api/bussiness/oauth/controller/KakaoOauthController.java
+++ src/main/java/kr/co/petmates/api/bussiness/oauth/controller/KakaoOauthController.java
@@ -1,20 +1,25 @@
 // KakaoOauthController는 사용자의 인가 코드를 처리하고, JWT 토큰을 생성하여 반환하는 컨트롤러입니다.
 package kr.co.petmates.api.bussiness.oauth.controller;
 
+import java.util.Map;
 import kr.co.petmates.api.bussiness.oauth.service.KakaoOauthService;
 import kr.co.petmates.api.bussiness.oauth.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // 이 클래스를 REST 컨트롤러로 선언합니다.
 @RequestMapping("/api/kakao") // 이 컨트롤러의 모든 매핑은 "/api/kakao" 경로로 시작합니다.
 public class KakaoOauthController {
+
+    private static final Logger logger = LoggerFactory.getLogger(KakaoOauthController.class);
 
     @Autowired // 스프링의 의존성 주입 기능을 사용하여 KakaoOauthService 객체를 자동으로 주입합니다.
     private KakaoOauthService kakaoOauthService;
@@ -28,10 +33,14 @@ public class KakaoOauthController {
     //    return ResponseEntity.ok("{login : user}"); // 간단한 문자열을 응답으로 반환합니다.
 
     @PostMapping("/login") // "/login" 경로로 POST 요청이 오면 이 메소드를 실행합니다.
-    public ResponseEntity<?> kakaoLogin(@RequestParam("code") String authorizationCode) {
+//    public ResponseEntity<?> kakaoLogin(@RequestParam("code") String code) {
 //    public ResponseEntity<?> kakaoLogin(@RequestBody String authorizationCode) { // 클라이언트로부터 받은 인가 코드를 매개변수로 받습니다.
-        // 인가 코드를 사용하여 액세스 토큰을 요청합니다.
-        String accessToken = kakaoOauthService.getAccessToken(authorizationCode);
+    public ResponseEntity<?> kakaoLogin(@RequestBody Map<String, String> requestBody) {
+        String code = requestBody.get("code");
+//        logger.info("abc " + code);
+  // code = null 인 경우 체크,
+    // 인가 코드를 사용하여 액세스 토큰을 요청합니다.
+        String accessToken = kakaoOauthService.getAccessToken(code);
         // 받은 액세스 토큰으로 JWT 토큰을 생성합니다.
         String jwtToken = userService.createJwtToken(accessToken);
         // 액세스 토큰을 사용하여 사용자 정보를 요청하고 결과를 가져옵니다.
