b792c88c4c3f1b1a1fa9a1dab2a111d0c6cbbb73
jhrogh <wogml5031@gmail.com>
2024-02-10 18:11:22 +0900

    feat(#28): 세션에 있는 엑세스토큰을 이용한 로그아웃으로 수정

--- src/main/java/kr/co/petmates/api/bussiness/oauth/controller/LogoutController.java
+++ src/main/java/kr/co/petmates/api/bussiness/oauth/controller/LogoutController.java
@@ -1,16 +1,12 @@
 package kr.co.petmates.api.bussiness.oauth.controller;
 
-import java.util.Optional;
-import kr.co.petmates.api.bussiness.oauth.config.JwtTokenProvider;
-import kr.co.petmates.api.bussiness.oauth.domain.User;
-import kr.co.petmates.api.bussiness.oauth.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
 import kr.co.petmates.api.bussiness.oauth.service.LogoutService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
@@ -21,27 +17,28 @@ public class LogoutController {
     private static final Logger logger = LoggerFactory.getLogger(KakaoOauthController.class);
     @Autowired
     private LogoutService logoutService;
-    @Autowired
-    private JwtTokenProvider jwtTokenProvider;
-    @Autowired
-    private UserRepository userRepository;
 
     @PostMapping("/logout")
-    public RedirectView logout(@RequestHeader("Authorization") String jwtToken,
-                               @RequestHeader("Refresh-Token") String refreshToken) {
-        jwtToken = jwtToken.replace("Bearer ", "");
-        logger.info("로그아웃 jwtToken: {}", jwtToken);
-        // 카카오 로그아웃 API 호출
-//        logoutService.logoutKakao(jwtToken);
-        String email = jwtTokenProvider.getEmail(refreshToken);
-        Optional<User> user = userRepository.findByEmail(email);
-        String getRefreshToken
-        // refreshToken 만료 처리
-        logoutService.invalidateRefreshToken(refreshToken);
+    public RedirectView logout(HttpSession session) {
+        logoutService.logout(session);
 
         RedirectView redirectView = new RedirectView("http://localhost:3000/main");
         redirectView.setStatusCode(HttpStatus.FOUND); // 여기에서 상태 코드를 설정합니다.
         return redirectView;
+//            , @RequestHeader("Authorization") String jwtToken, @RequestHeader("Refresh-Token") String refreshToken) {
+//        jwtToken = jwtToken.replace("Bearer ", "");
+//        logger.info("로그아웃 jwtToken: {}", jwtToken);
+//        // 카카오 로그아웃 API 호출
+////        logoutService.logoutKakao(jwtToken);
+//        String email = jwtTokenProvider.getEmail(refreshToken);
+//        Optional<User> user = userRepository.findByEmail(email);
+//        String getRefreshToken
+//        // refreshToken 만료 처리
+//        logoutService.kakaoLogout(refreshToken);
+//
+//        RedirectView redirectView = new RedirectView("http://localhost:3000/main");
+//        redirectView.setStatusCode(HttpStatus.FOUND); // 여기에서 상태 코드를 설정합니다.
+//        return redirectView;
         // 처리 완료 후 리다이렉트
 //        return new RedirectView("http://localhost:3000/main", true, HttpStatus.FOUND.value(), false);
     }
--- src/main/java/kr/co/petmates/api/bussiness/oauth/service/LogoutService.java
+++ src/main/java/kr/co/petmates/api/bussiness/oauth/service/LogoutService.java
@@ -1,10 +1,11 @@
 package kr.co.petmates.api.bussiness.oauth.service;
 
+import jakarta.servlet.http.HttpSession;
 import kr.co.petmates.api.bussiness.oauth.controller.KakaoOauthController;
-import kr.co.petmates.api.bussiness.oauth.domain.User;
 import kr.co.petmates.api.bussiness.oauth.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -16,6 +17,8 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class LogoutService {
     private static final Logger logger = LoggerFactory.getLogger(KakaoOauthController.class);
+    @Autowired
+    private AccessTokenStorage accessTokenStorage; // AccessTokenStorage 추가
     @Value("${kakao.logout}") // application.properties 파일에서 kakao.logout.url 값을 주입받습니다.
     private String kakaoLogoutUrl; // 카카오 로그아웃 API의 URL을 저장하는 변수입니다.
 
@@ -27,7 +30,16 @@ public class LogoutService {
         this.userRepository = userRepository;
     }
 
-    public void logoutKakao(String accessToken) {
+    public void logout(HttpSession session) {
+        String accessToken = accessTokenStorage.getAccessToken(session);
+        kakaoLogout(accessToken); // 이 메소드는 실제 카카오 로그아웃 API를 호출하는 구현이 필요합니다.
+
+        // 세션 무효화
+        session.invalidate();
+        logger.info("카카오 로그아웃 완료");
+    }
+
+    public void kakaoLogout(String accessToken) {
         RestTemplate restTemplate = new RestTemplate(); // RESTful API 호출을 위한 RestTemplate 인스턴스를 생성합니다.
         HttpHeaders headers = new HttpHeaders(); // HTTP 요청을 위한 헤더 객체를 생성합니다.
         headers.setBearerAuth(accessToken); // Bearer 토큰을 사용하여 인증 헤더를 설정합니다.
@@ -36,17 +48,33 @@ public class LogoutService {
         // 카카오 로그아웃 API를 호출합니다. ResponseEntity 객체를 사용하여 응답을 받습니다.
         ResponseEntity<String> response = restTemplate.exchange(
                 kakaoLogoutUrl, HttpMethod.POST, entity, String.class);
-
-        logger.info("카카오 로그아웃 완료");
     }
 
-    public void invalidateRefreshToken(String refreshToken) {
-        // refreshToken 값을 기준으로 데이터베이스에서 리프레시 토큰 객체를 조회합니다.
-        User token = userRepository.findByRefreshToken(refreshToken)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid refresh token")); // 토큰이 없는 경우 예외를 발생시킵니다.
 
-        // 조회된 리프레시 토큰의 만료 상태를 true(만료됨)로 설정합니다.
-        token.setRefreshToken(null); // refreshToken 필드를 null로 설정하여 토큰을 무효화
-        userRepository.save(token); // 수정된 리프레시 토큰 객체를 데이터베이스에 저장합니다.
-    }
+
+
+
+
+//    public void logoutKakao(String accessToken) {
+//        RestTemplate restTemplate = new RestTemplate(); // RESTful API 호출을 위한 RestTemplate 인스턴스를 생성합니다.
+//        HttpHeaders headers = new HttpHeaders(); // HTTP 요청을 위한 헤더 객체를 생성합니다.
+//        headers.setBearerAuth(accessToken); // Bearer 토큰을 사용하여 인증 헤더를 설정합니다.
+//        HttpEntity<String> entity = new HttpEntity<>(headers); // HTTP 요청 본문 없이 헤더만 포함하는 HttpEntity 객체를 생성합니다.
+//
+//        // 카카오 로그아웃 API를 호출합니다. ResponseEntity 객체를 사용하여 응답을 받습니다.
+//        ResponseEntity<String> response = restTemplate.exchange(
+//                kakaoLogoutUrl, HttpMethod.POST, entity, String.class);
+//
+//        logger.info("카카오 로그아웃 완료");
+//    }
+//
+//    public void invalidateRefreshToken(String refreshToken) {
+//        // refreshToken 값을 기준으로 데이터베이스에서 리프레시 토큰 객체를 조회합니다.
+//        User token = userRepository.findByRefreshToken(refreshToken)
+//                .orElseThrow(() -> new IllegalArgumentException("Invalid refresh token")); // 토큰이 없는 경우 예외를 발생시킵니다.
+//
+//        // 조회된 리프레시 토큰의 만료 상태를 true(만료됨)로 설정합니다.
+//        token.setRefreshToken(null); // refreshToken 필드를 null로 설정하여 토큰을 무효화
+//        userRepository.save(token); // 수정된 리프레시 토큰 객체를 데이터베이스에 저장합니다.
+//    }
 }
