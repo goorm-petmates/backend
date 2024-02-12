7744e556fb1c043341611abc450d1555f222ccac
jhrogh <wogml5031@gmail.com>
2024-02-13 03:38:02 +0900

    feat(#29): jwt 토큰 세션쿠키로 수정 및 전체 1차 테스트 완료

--- src/main/java/kr/co/petmates/api/bussiness/oauth/controller/KakaoOauthController.java
+++ src/main/java/kr/co/petmates/api/bussiness/oauth/controller/KakaoOauthController.java
@@ -2,8 +2,10 @@
 package kr.co.petmates.api.bussiness.oauth.controller;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import kr.co.petmates.api.bussiness.oauth.client.KakaoApiClient;
@@ -15,6 +17,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -67,6 +70,7 @@ public class KakaoOauthController {
         jwtTokenCookie.setPath("/");
         jwtTokenCookie.setMaxAge(-1); // 세션 쿠키로 설정하여 브라우저 종료 시 삭제
         response.addCookie(jwtTokenCookie);
+        logger.info("컨테이너 로그인 jwtTokenCookie: {}", jwtTokenCookie);
 
         // 리프레시 토큰을 세션 쿠키에 저장
         Cookie refreshTokenCookie = new Cookie("refreshToken", authResult.getRefreshToken());
@@ -78,12 +82,40 @@ public class KakaoOauthController {
         // isNewUser 정보는 응답 본문에 포함
         Map<String, Object> responseBody = new HashMap<>();
         responseBody.put("isNewUser", authResult.isNewUser());
-
+        logger.info("컨테이너 isNewUser: {}", authResult.isNewUser());
+        logger.info("컨테이너 return: {}", ResponseEntity.ok(responseBody));
         return ResponseEntity.ok(responseBody);
     }
 
+    @GetMapping("/login/status")
+    public ResponseEntity<?> checkAuthStatus(HttpServletRequest request) {
+        // 쿠키에서 토큰을 검사하고 로그인 상태 확인 로직 구현
+        boolean isLoggedIn = checkLoginStatus(request);
+        logger.info("컨테이너 로그인 상태 체크 isLoggedIn: {}", isLoggedIn);
+        // 로그인 상태를 클라이언트에게 반환
+        return ResponseEntity.ok().body(Collections.singletonMap("isLoggedIn", isLoggedIn));
+    }
+
+    private boolean checkLoginStatus(HttpServletRequest request) {
+        // 쿠키에서 토큰 존재 여부 확인, 토큰의 유효성 검증 등을 수행
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {    // "jwtToken" 쿠키를 찾아 값이 비어있지 않은지 확인
+                if ("jwtToken".equals(cookie.getName()) && cookie.getValue() != null && !cookie.getValue().isEmpty()) {
+                    // 쿠키에 "jwtToken"이 존재하고 값이 비어있지 않으면 로그인 상태로 간주
+                    logger.info("컨테이너 쿠키 JwtToken 있음");
+                    return true;
+                }
+            }
+        }
+        // "jwtToken" 쿠키가 없거나 값이 비어있으면 로그인 상태가 아님
+        logger.info("컨테이너 쿠키 JwtToken 없음");
+        return false;
+    }
+
+
     @PostMapping("/logout")
-    public ResponseEntity<?> logout(HttpSession session) {
+    public ResponseEntity<?> logout(HttpServletResponse response, HttpSession session) {
 
         boolean isKakaoLogout = kakaoApiClient.kakaoLogout(session);
         logger.info("로그아웃 성공 여부: {}", isKakaoLogout);
@@ -91,19 +123,34 @@ public class KakaoOauthController {
         if (isKakaoLogout) {
             session.invalidate();
 
-            Map<String, Object> response = new HashMap<>();
-            response.put("result", "success");
-            return ResponseEntity.ok(response);
+            // JWT 토큰이 저장된 쿠키 삭제
+            Cookie jwtTokenCookie = new Cookie("jwtToken", null); // 쿠키 이름에 해당하는 새 쿠키 생성
+            jwtTokenCookie.setPath("/");
+            jwtTokenCookie.setMaxAge(0); // 쿠키의 유효기간을 0으로 설정하여 즉시 만료
+            jwtTokenCookie.setHttpOnly(true); // HttpOnly 설정 유지
+            response.addCookie(jwtTokenCookie); // 응답에 쿠키 추가하여 클라이언트에게 전송
+            logger.info("컨테이너 로그아웃 jwtTokenCookie 삭제?: {}", jwtTokenCookie);
+            // refresh 토큰이 저장된 쿠키 삭제
+            Cookie refreshTokenCookie = new Cookie("refreshToken", null); // 쿠키 이름에 해당하는 새 쿠키 생성
+            refreshTokenCookie.setPath("/");
+            refreshTokenCookie.setMaxAge(0); // 쿠키의 유효기간을 0으로 설정하여 즉시 만료
+            refreshTokenCookie.setHttpOnly(true); // HttpOnly 설정 유지
+            response.addCookie(refreshTokenCookie); // 응답에 쿠키 추가하여 클라이언트에게 전송
+
+            Map<String, Object> responseBody = new HashMap<>();
+            responseBody.put("result", "success");
+            return ResponseEntity.ok(responseBody);
         } else {
-            Map<String, Object> response = new HashMap<>();
-            response.put("result", "failed");
-            response.put("data","카카오 엑세스토큰 만료");
-            return ResponseEntity.ok(response);
+            Map<String, Object> responseBody = new HashMap<>();
+            responseBody.put("result", "failed");
+            responseBody.put("data","카카오 엑세스토큰 만료");
+//            return ResponseEntity.ok(responseBody);
+            return ResponseEntity.badRequest().body(responseBody);
         }
     }
 
     @PostMapping("/logout/update")
-    public ResponseEntity<?> logoutUpdate(HttpSession session) {
+    public ResponseEntity<?> logoutUpdateKakaoToken(HttpSession session) {
         String accessToken = accessTokenStorage.getAccessToken(session);
         logger.info("로그아웃 갱신 전 엑세스토큰: {}", accessToken);
         String refreshToken = accessTokenStorage.getRefreshToken(session);
@@ -114,9 +161,9 @@ public class KakaoOauthController {
 
         session.invalidate();
 
-        Map<String, Object> response = new HashMap<>();
-        response.put("result", "success");
-        logger.info("로그아웃 카카오토큰 갱신 return: {}", ResponseEntity.ok(response));
-        return ResponseEntity.ok(response);
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("result", "success");
+        logger.info("로그아웃 카카오토큰 갱신 return: {}", ResponseEntity.ok(responseBody));
+        return ResponseEntity.ok(responseBody);
     }
 }
--- src/main/java/kr/co/petmates/api/bussiness/oauth/controller/TokenUpdateController.java
+++ src/main/java/kr/co/petmates/api/bussiness/oauth/controller/TokenUpdateController.java
@@ -4,8 +4,9 @@ package kr.co.petmates.api.bussiness.oauth.controller;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import java.util.HashMap;
-import java.util.Map;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import javax.crypto.SecretKey;
 import kr.co.petmates.api.bussiness.oauth.config.JwtTokenProvider;
 import kr.co.petmates.api.bussiness.oauth.service.TokenCheckService;
@@ -15,7 +16,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,35 +31,88 @@ public class TokenUpdateController {
     private TokenCheckService tokenCheckService; // 토큰 갱신 로직을 담당하는 서비스를 주입합니다.
 
     @PostMapping("/update") // "/api/oauth/jwt/update" 경로로 POST 요청을 받는 메소드를 정의합니다.
-    public ResponseEntity<?> tokenUpdate(@RequestBody Map<String, String> request) {
+    public ResponseEntity<?> tokenUpdate(HttpServletResponse response, HttpServletRequest request) {
         // 요청 본문에서 "jwtToken"와 "refreshToken"을 추출하여 토큰 갱신 서비스를 호출합니다.
-        String jwtToken = request.get("jwtToken");
-        String refreshToken = request.get("refreshToken");
+//        String jwtToken = requestBody.get("jwtToken");
+//        String refreshToken = requestBody.get("refreshToken");
+        String jwtToken = null;
+        String refreshToken = null;
+        logger.info("토큰갱신 초기 jwtToken: {}", jwtToken);
+
+        // 요청으로부터 쿠키 배열을 가져옵니다.
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("jwtToken".equals(cookie.getName())) { // "jwtToken" 쿠키를 찾습니다.
+                    jwtToken = cookie.getValue();
+                    logger.info("토큰갱신 쿠키에서 찾은 jwtToken: {}", jwtToken);
+                } else if ("refreshToken".equals(cookie.getName())) { // "refreshToken" 쿠키를 찾습니다.
+                    refreshToken = cookie.getValue();
+                }
+            }
+        }
+
+        if (jwtToken == null || refreshToken == null) {
+            return ResponseEntity.badRequest().body("Token not found in cookies"); // 쿠키에서 토큰을 찾지 못한 경우 오류 응답
+        }
+
 
         Claims claims = Jwts.parser()
                 .setSigningKey(secretKey)
                 .parseClaimsJws(jwtToken)
                 .getBody();
         String email = claims.getSubject();
+        logger.info("토큰갱신 찾은 email: {}", email);
         String newJwtToken = jwtTokenProvider.createJwtToken(email);
+        logger.info("토큰갱신 갱신한 새로운 jwtToken: {}", newJwtToken);
+        // 새로운 JWT 토큰을 쿠키에 저장
+        Cookie newJwtTokenCookie = new Cookie("jwtToken", newJwtToken);
+        newJwtTokenCookie.setHttpOnly(true);
+        newJwtTokenCookie.setPath("/");
+        newJwtTokenCookie.setMaxAge(-1);
+        response.addCookie(newJwtTokenCookie);
+        logger.info("토큰갱신 갱신 후 쿠키저장 newJwtTokenCookie: {}", newJwtTokenCookie);
 
         boolean isValidateRefreshToken = jwtTokenProvider.validateToken(refreshToken);
         logger.info("refreshToken 유효성: {}", isValidateRefreshToken);
 
-        Map<String, String> response = new HashMap<>();
-
-        if(isValidateRefreshToken){
-            response.put("jwtToken", newJwtToken);
-            response.put("refreshToken", refreshToken);
-            logger.info("refreshToken 유효성 : true");
-        } else {
+        if (!isValidateRefreshToken) {
             String newRefreshToken = jwtTokenProvider.createRefreshToken(newJwtToken);
-            response.put("jwtToken", newJwtToken);
-            response.put("refreshToken", newRefreshToken);
-            logger.info("refreshToken 유효성 : false");
+
+            // 새로운 리프레시 토큰을 쿠키에 저장
+            Cookie newRefreshTokenCookie = new Cookie("refreshToken", newRefreshToken);
+            newRefreshTokenCookie.setHttpOnly(true);
+            newRefreshTokenCookie.setPath("/");
+            newRefreshTokenCookie.setMaxAge(-1);
+            response.addCookie(newRefreshTokenCookie);
+
         }
-        logger.info("최종 토큰: {}", ResponseEntity.ok(response));
-        // newJwtToken 변수를 반환합니다.
-        return ResponseEntity.ok(response);
+
+// 응답 본문에 토큰 정보를 포함하지 않고, 쿠키를 통해 반환
+        return ResponseEntity.ok().build();
+
+
+
+
+
+
+
+
+
+//        Map<String, String> responseBody = new HashMap<>();
+//
+//        if(isValidateRefreshToken){
+//            responseBody.put("jwtToken", newJwtToken);
+//            responseBody.put("refreshToken", refreshToken);
+//            logger.info("refreshToken 유효성 : true");
+//        } else {
+//            String newRefreshToken = jwtTokenProvider.createRefreshToken(newJwtToken);
+//            responseBody.put("jwtToken", newJwtToken);
+//            responseBody.put("refreshToken", newRefreshToken);
+//            logger.info("refreshToken 유효성 : false");
+//        }
+//        logger.info("최종 토큰: {}", ResponseEntity.ok(responseBody));
+//        // newJwtToken 변수를 반환합니다.
+//        return ResponseEntity.ok(responseBody);
     }
 }
