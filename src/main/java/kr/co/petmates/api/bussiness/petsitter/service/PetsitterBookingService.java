e6deb6bac49941e0f0b45639d34ba04d3603ff02
hyorim-luna-choi <hyolimjj@naver.com>
2024-02-14 15:33:08 +0900

    feat(#25) : 예약 요청 조회 - 예약 요청 조회 기능 보완

--- src/main/java/kr/co/petmates/api/bussiness/petsitter/service/PetsitterBookingService.java
+++ src/main/java/kr/co/petmates/api/bussiness/petsitter/service/PetsitterBookingService.java
@@ -1,8 +1,7 @@
 package kr.co.petmates.api.bussiness.petsitter.service;
 
-import java.util.stream.Collectors;
 import kr.co.petmates.api.bussiness.pet.entity.BookedPet;
-import kr.co.petmates.api.bussiness.pet.repository.BookedPetRepository;
+import kr.co.petmates.api.bussiness.pet.entity.Pet;
 import kr.co.petmates.api.bussiness.petsitter.repository.PetsitterBookingRepository;
 import kr.co.petmates.api.bussiness.reserve.dto.BookingDto;
 import kr.co.petmates.api.bussiness.reserve.entity.Booking;
@@ -11,14 +10,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import java.util.List;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class PetsitterBookingService {
     private final PetsitterBookingRepository petsitterBookingRepository;
-    private final BookedPetRepository bookedPetRepository;
 
     // 펫시터의 예약 요청 리스트 조회 -> BOOK_REQUEST, BOOK_APPROVED, BOOK_SUCCESS, BOOK_COMPLETE 에 해당하는 예약들을 조회
     @Transactional
@@ -30,9 +27,12 @@ public class PetsitterBookingService {
         return bookingRequestPage.map(booking -> {
             BookingDto bookingDto = BookingDto.toBookingDto(booking);
 
-            // 예약에 연결된 펫의 ID를 가져옵니다.
-            Long bookedPetId = booking.getBookedPet().iterator().next().getPet().getId();
-            bookingDto.setBookedPetId(bookedPetId); // 해당 펫의 ID를 설정합니다.
+            Long bookedPetId = booking.getBookedPet().stream()
+                    .findFirst() // 첫 번째 BookedPet 객체를 가져옵니다.
+                    .map(BookedPet::getPet) // BookedPet에서 Pet 객체를 얻습니다.
+                    .map(Pet::getId) // Pet 객체에서 ID를 얻습니다.
+                    .orElse(null); // 만약 BookedPet 객체가 없다면 null을 반환합니다.
+            bookingDto.setBookedPetId(bookedPetId);
 
             return bookingDto;
         });
